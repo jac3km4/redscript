5d4a90a6b39c1d6da84263a2980612e390b1e944
jekky <11986158+jac3km4@users.noreply.github.com>
2021-01-06 18:20:58 +0000

    Cleanup

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,7 +1,6 @@
 use redscript::bundle::ScriptBundle;
 use redscript::definition::DefinitionValue;
 use redscript::error::Error;
-use redscript::files::FileIndex;
 use redscript::print::{write_definition, OutputMode};
 
 use gumdrop::Options;
@@ -17,12 +16,20 @@ struct Configuration {
     output: PathBuf,
     #[options(short = "m", help = "dump mode (one of: 'ast', 'bytecode' or 'code')")]
     mode: String,
-    #[options(short = "f", help = "output individual files (doesn't work for everything yet)")]
+    #[options(short = "f", help = "split into individual files (doesn't work for everything yet)")]
     dump_files: bool,
 }
 
 fn main() -> Result<(), Error> {
-    let config: Configuration = Configuration::parse_args_default_or_exit();
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let config: Configuration = match Configuration::parse_args_default(&args) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("{}", err);
+            println!("{}", Configuration::usage());
+            return Ok(());
+        }
+    };
 
     let cache: ScriptBundle = ScriptBundle::load(&mut BufReader::new(File::open(config.input)?))?;
 
@@ -35,19 +42,19 @@ fn main() -> Result<(), Error> {
     let pool = cache.pool();
 
     if config.dump_files {
-        for entry in FileIndex::from_pool(pool).files() {
-            let path = config.output.join(&entry.file.path);
+        for entry in pool.files().iter() {
+            let path = config.output.as_path().join(&entry.file.path);
 
             std::fs::create_dir_all(path.parent().unwrap())?;
             let mut output = BufWriter::new(File::create(path)?);
             for def in entry.definitions {
                 if let Err(err) = write_definition(&mut output, def, pool, 0, mode) {
-                    println!("Failed to process definition {:?}: {:?}", def, err);
+                    println!("Failed to process definition at {}: {:?}", def.file_offset, err);
                 }
             }
         }
     } else {
-        let mut output = BufWriter::new(File::create(config.output).expect("Failed to create output file"));
+        let mut output = BufWriter::new(File::create(&config.output)?);
 
         for (_, def) in pool.roots().filter(|(_, def)| {
             matches!(&def.value, DefinitionValue::Class(_))
@@ -55,10 +62,10 @@ fn main() -> Result<(), Error> {
                 || matches!(&def.value, DefinitionValue::Function(_))
         }) {
             if let Err(err) = write_definition(&mut output, def, pool, 0, mode) {
-                println!("Failed to process definition {:?}: {:?}", def, err);
+                println!("Failed to process definition at {}: {:?}", def.file_offset, err);
             }
         }
     }
-    println!("Done");
+    println!("Output successfully saved to {:?}", config.output);
     Ok(())
 }
