93379643a01854a3ef8242084328dfa398dbaa21
jekky <11986158+jac3km4@users.noreply.github.com>
2022-12-23 09:21:01 +0100

    chore: clean up scc main

--- scc/src/hints.rs
+++ scc/src/hints.rs
@@ -6,7 +6,7 @@ use std::path::{Path, PathBuf};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-pub struct UserAction {
+pub struct UserHint {
     pub id: String,
     pub message: String,
     pub file: Option<PathBuf>,
@@ -15,30 +15,30 @@ pub struct UserAction {
 }
 
 #[derive(Debug, Default)]
-pub struct UserActions {
-    pub actions: HashMap<String, Vec<UserAction>>,
+pub struct UserHints {
+    pub hints: HashMap<String, Vec<UserHint>>,
 }
 
-impl UserActions {
+impl UserHints {
     pub fn load(path: impl AsRef<Path>) -> Result<Self, Box<dyn Error>> {
-        let mut actions = HashMap::new();
+        let mut hints = HashMap::new();
         if !path.as_ref().exists() {
-            return Ok(Self { actions });
+            return Ok(Self { hints });
         }
         let dir = std::fs::read_dir(path)?;
         for entry in dir {
             let entry = entry?;
             if entry.path().extension() == Some(OsStr::new("toml")) {
                 let contents = std::fs::read_to_string(entry.path())?;
-                let contents: HashMap<String, Vec<UserAction>> = toml::from_str(&contents)?;
-                actions.extend(contents);
+                let contents: HashMap<String, Vec<UserHint>> = toml::from_str(&contents)?;
+                hints.extend(contents);
             }
         }
-        Ok(Self { actions })
+        Ok(Self { hints })
     }
 
-    pub fn get_by_error(&self, error_code: &str, path: &Path, source: &str, source_line: &str) -> Option<&UserAction> {
-        self.actions.get(error_code)?.iter().find(|a| {
+    pub fn get_by_error(&self, error_code: &str, path: &Path, source: &str, source_line: &str) -> Option<&UserHint> {
+        self.hints.get(error_code)?.iter().find(|a| {
             a.file.as_ref().map_or(true, |p| p == path)
                 && (matches!(&a.span_starts_with, Some(str) if source.starts_with(str))
                     || matches!(&a.line_contains, Some(str) if source_line.contains(str)))
