45da06de4dd7014eaffed7d534d31850762d15fa
Jack Humbert <jack.humb@gmail.com>
2023-04-30 11:44:49 -0400

    adds more cybercmd tests

--- scc/tests/args.rs
+++ scc/tests/args.rs
@@ -73,11 +73,56 @@ fn test_order(f_args: &[TestArg]) {
     assert_eq!(opts.threads, 4);
 }
 
-mod tests {
+fn test_order_cyber(f_args: &[TestArg]) {
+    let args = test_fix_args(
+        &[
+            concat!(
+                r#"-compile "C:\Program Files (x86)\Steam\steamapps\common\Cyberpunk 2077\r6\scripts""#,
+                r#" -customCacheDir "C:\Program Files (x86)\Steam\steamapps\common\Cyberpunk 2077\r6\cache\modded""#,
+            ), 
+            &f_args.iter().map(|a| a.raw).collect::<Vec<_>>().join(""),
+        ]
+        .join(""),
+        &[
+            &[
+                "-compile",
+                "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\scripts",
+                "-customCacheDir",
+                "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\cache\\modded",
+            ],
+            f_args
+                .iter()
+                .flat_map(|a| ["-compile", a.parsed])
+                .collect::<Vec<_>>()
+                .as_slice(),
+        ]
+        .concat(),
+    )
+    .unwrap();
+    let opts = Opts::load(args.iter().map(String::as_str).collect::<Vec<&str>>().as_slice());
+
+    assert_eq!(opts.script_paths,
+        [PathBuf::from(
+            "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\scripts"
+        )]
+        .into_iter()
+        .chain(f_args.iter().map(|a| PathBuf::from(a.parsed)))
+        .collect::<Vec<_>>(),
+        "Testing script_paths"
+    );
+    assert_eq!(opts.cache_file, None, "Testings cache_file");
+    assert_eq!(
+        opts.cache_dir, 
+        Some("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\cache\\modded".into()),
+        "Testing cache_dir"
+    );
+}
+
+mod standard {
     use super::*;
 
     #[test]
-    fn test_standard() {
+    fn test_empty() {
         test_order(&[]);
     }
     #[test]
@@ -136,25 +181,69 @@ mod tests {
     fn test_dffdf() {
         test_order(&[TEST_DIRECTORY, TEST_FILE, TEST_FILE, TEST_DIRECTORY, TEST_FILE]);
     }
+}
+
+mod cybercmd {
+    use super::*;
 
     #[test]
-    fn test_cybercmd() {
-        let opts = Opts::load(&[
-            "-compile",
-            "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\scripts",
-            "-customCacheDir",
-            "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\cache\\modded",
-        ]);
-        assert_eq!(
-            opts.script_paths,
-            &[PathBuf::from(
-                "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\scripts"
-            )]
-        );
-        assert_eq!(
-            opts.cache_dir,
-            Some("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Cyberpunk 2077\\r6\\cache\\modded".into())
-        );
-        assert_eq!(opts.cache_file, None);
+    fn test_empty() {
+        test_order_cyber(&[]);
+    }
+    #[test]
+    fn test_fd() {
+        test_order_cyber(&[TEST_FILE, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_fdf() {
+        test_order_cyber(&[TEST_FILE, TEST_FILE, TEST_FILE]);
+    }
+    #[test]
+    fn test_ffd() {
+        test_order_cyber(&[TEST_FILE, TEST_FILE, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_fdd() {
+        test_order_cyber(&[TEST_FILE, TEST_DIRECTORY, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_fddf() {
+        test_order_cyber(&[TEST_FILE, TEST_DIRECTORY, TEST_DIRECTORY, TEST_FILE]);
+    }
+    #[test]
+    fn test_fdfd() {
+        test_order_cyber(&[TEST_FILE, TEST_DIRECTORY, TEST_DIRECTORY, TEST_FILE]);
+    }
+    #[test]
+    fn test_fddfd() {
+        test_order_cyber(&[TEST_FILE, TEST_DIRECTORY, TEST_DIRECTORY, TEST_FILE, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_df() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_FILE]);
+    }
+    #[test]
+    fn test_dfd() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_FILE, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_ddf() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_DIRECTORY, TEST_FILE]);
+    }
+    #[test]
+    fn test_dff() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_FILE, TEST_FILE]);
+    }
+    #[test]
+    fn test_dffd() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_FILE, TEST_FILE, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_dfdf() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_FILE, TEST_FILE, TEST_DIRECTORY]);
+    }
+    #[test]
+    fn test_dffdf() {
+        test_order_cyber(&[TEST_DIRECTORY, TEST_FILE, TEST_FILE, TEST_DIRECTORY, TEST_FILE]);
     }
 }
